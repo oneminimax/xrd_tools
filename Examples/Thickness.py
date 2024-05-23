bb325fa9f16089e260e0f59bcc9d91d337e46417
Maxime Dion <maxime.dion@me.com>
2024-05-23 09:11:57 -0400

    Clean up

--- Examples/Thickness.py
+++ Examples/Thickness.py
@@ -1,83 +1,83 @@
-# from os import path
+# %%
 
-# import matplotlib
-# import numpy as np
+from os import path
 
-# matplotlib.use("Qt5Agg")
-# import matplotlib.pyplot as plt
-# from AsciiDataFile.Readers import GenericDataReader as Reader
-# from lmfit import Model
-# from XRDTools.Diffractometer import Diffractometer
-# from XRDTools.Functions import ag_fct
-# from XRDTools.Geometry import two_theta_2_wave_vector_length
-# from XRDTools.Spectrum import calculate_ag, centroid, estimate_noise_level
+import matplotlib.pyplot as plt
+import numpy as np
+from lmfit import Model
 
-# data_path = "Data/"
+from xrd_tools.diffractometer import Diffractometer
+from xrd_tools.functions import ag_fct
+from xrd_tools.geometry import two_theta_2_wave_vector_length
+from xrd_tools.spectrum import calculate_ag, centroid, estimate_noise_level
 
-# sample_tuples = [
-#     # ('T2T_22_24_STO.dat',(22,24)),
-#     # ('T2T_22_23p5_LSAT.dat',(22,23.5)),
-#     ("thickness_PCCO.dat", (28, 30.5))
-# ]
+data_path = "Data/"
 
-# dm = Diffractometer()
-# wave_length = dm.wave_length
+sample_tuples = [
+    # ('T2T_22_24_STO.dat',(22,24)),
+    # ('T2T_22_23p5_LSAT.dat',(22,23.5)),
+    ("thickness_PCCO.dat", (28, 30.5))
+]
 
-# fig = plt.figure()
-# AX1 = fig.add_subplot(1, 2, 1)
-# AX2 = fig.add_subplot(2, 2, 2)
-# AX3 = fig.add_subplot(2, 2, 4)
+dm = Diffractometer()
+wave_length = dm.wave_length
 
-# AX1.set_yscale("log")
-# AX3.set_yscale("log")
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2)
+ax3 = fig.add_subplot(2, 2, 4)
 
-# # ag_fct(x,wave_vector_g,thickness,sigma_eps,scale,noise)
-# ag_model = Model(ag_fct)
-# params = ag_model.make_params()
-# params["wave_vector_g"].vary = False
-# params["thickness"].value = 60
-# params["sigma_eps"].value = 1e-5
-# params["scale"].value = 1
-# params["noise"].value = 1e-6
+ax1.set_yscale("log")
+ax3.set_yscale("log")
 
-# for sample_tuple in sample_tuples:
-#     file_name = sample_tuple[0]
-#     two_theta_lim = sample_tuple[1]
-#     reader = Reader(" ", ["two theta", "signal"])
-#     CS = reader.read(path.join(data_path, file_name))
+ax1.set_xlabel("Angle")
+ax1.set_ylabel("Intensity")
 
-#     CS.add_column("noise", "count", estimate_noise_level(CS.get_column("two theta"), CS.get_column("signal")))
+ax3.set_xlabel("Distance (nm)")
+ax2.set_ylabel("AG(x)")
 
-#     subCS = CS.select_range("two theta", two_theta_lim, new_curve=True)
+ag_model = Model(ag_fct)
+params = ag_model.make_params()
+params["wave_vector_g"].vary = False
+params["thickness"].value = 60
+params["sigma_eps"].value = 1e-5
+params["scale"].value = 1
+params["noise"].value = 1e-6
 
-#     two_theta = subCS.get_column("two theta")
-#     signal = subCS.get_column("signal")
-#     noise = subCS.get_column("noise")
-#     signal_error = 1 * np.ones(signal.shape)
+for sample_tuple in sample_tuples:
+    file_name = sample_tuple[0]
+    two_theta_lim = sample_tuple[1]
 
-#     wave_vector = two_theta_2_wave_vector_length(wave_length, two_theta)
-#     wave_vector_g = centroid(wave_vector, signal)
-#     params["wave_vector_g"].value = wave_vector_g
+    data = np.loadtxt(path.join(data_path, file_name), skiprows=0)
 
-#     x, ag, abs_ag_error, arg_ag_error = calculate_ag(
-#         wave_vector - wave_vector_g, signal - noise, signal_error, x_max=120, x_step=2
-#     )
-#     abs_ag = np.abs(ag)
+    two_thetas = data[:, 0]
+    intensities = data[:, 1]
+    noises = estimate_noise_level(two_thetas, intensities)
+    signal_error = 1 * np.ones(intensities.shape)
 
-#     result = ag_model.fit(abs_ag, params, x=x, method="least_squares")
-#     dely = result.eval_uncertainty(sigma=3)
+    wave_vector = two_theta_2_wave_vector_length(wave_length, two_thetas)
+    wave_vector_g = centroid(wave_vector, intensities)
+    params["wave_vector_g"].value = wave_vector_g
 
-#     print(result.fit_report())
+    x, ag, abs_ag_error, arg_ag_error = calculate_ag(
+        wave_vector - wave_vector_g, intensities - noises, signal_error, x_max=120, x_step=2
+    )
+    abs_ag = np.abs(ag)
 
-#     AX1.plot(two_theta, signal)
-#     AX1.plot(two_theta, noise, "--k")
+    result = ag_model.fit(abs_ag, params, x=x, method="least_squares")
+    dely = result.eval_uncertainty(sigma=3)
 
-#     AX2.plot(x, abs_ag)
-#     AX2.fill_between(x, result.best_fit - dely, result.best_fit + dely, color="#ABABAB")
-#     AX2.plot(x, result.best_fit)
+    print(result.fit_report())
 
-#     AX3.plot(x, abs_ag)
-#     AX3.fill_between(x, result.best_fit - dely, result.best_fit + dely, color="#ABABAB")
-#     AX3.plot(x, result.best_fit)
+    ax1.plot(two_thetas, intensities)
+    ax1.plot(two_thetas, noises, "--k")
 
-# plt.show()
+    ax2.plot(x, abs_ag)
+    ax2.fill_between(x, result.best_fit - dely, result.best_fit + dely, color="#ABABAB")
+    ax2.plot(x, result.best_fit)
+
+    ax3.plot(x, abs_ag)
+    ax3.fill_between(x, result.best_fit - dely, result.best_fit + dely, color="#ABABAB")
+    ax3.plot(x, result.best_fit)
+
+# %%
--- Examples/XiZeta_to_Alpha_Phi.py
+++
@@ -1,3 +0,0 @@
-# from XRDTools.Geometry import xi_zeta_2_alpha_phi
-
-# print(xi_zeta_2_alpha_phi(6.1,0.55))
